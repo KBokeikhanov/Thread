2d27c1c281929caf8e4843d4b45e089639ac7497
KBoke <KBokeikhanov@beeline.kz>
2022-03-04 15:50:56 +0600

    Thread

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+
+            MyFirstThread thread = new MyFirstThread();
+            thread.start();
+        }
+    }
+}
